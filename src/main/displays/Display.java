00e5c9a02f8b2ec44ac04144f438c7d587c6bd05
Luis Wilken <Luis@C-LW>
2023-12-16 22:53:42 +0100

    add antialiasing

--- src/main/displays/Display.java
+++ src/main/displays/Display.java
@@ -26,6 +26,9 @@ public abstract class Display extends JPanel implements IObserver<DisplayData> {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         draw(g);
     }
 }
